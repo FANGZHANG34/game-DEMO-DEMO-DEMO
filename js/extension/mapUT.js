4a4267e598cdac4b78fa7f0c2292b8ae3e6c9c10
方丈34 <168342791+FANGZHANG34@users.noreply.github.com>
2024-07-10 13:24:31 +0800

    Delete js directory

--- js/extension/mapUT.js
+++
@@ -1,4 +0,0 @@
-const mapArrayUT = new Map();
-mapArrayUT.
-set('_',['./img/bg.png','./img/林元.jpg','./img/林元.jpg',,'./img/林元.jpg']).
-forEach(function(value,key){this[value[0]] = key;},mapArrayUT.list = {});
